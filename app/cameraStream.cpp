c67a9d083eede3de0c05d0b2cfb8463c2212c82e
byfron <josecrubio82@gmail.com>
2016-11-26 13:16:02 +0100

    Adds example markers

--- app/cameraStream.cpp
+++ app/cameraStream.cpp
@@ -8,7 +8,13 @@ int main(int argc, char ** argv) {
 	
 	for (;;) {
 		camera.getFrame(frame);
-		cv::imshow("camera stream", frame);
+
+		cv::Mat grayFrame;
+		cvtColor(frame, grayFrame, CV_RGB2GRAY);
+		
+		cv::adaptiveThreshold(grayFrame, grayFrame, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY,11,2);
+		
+		cv::imshow("camera stream", grayFrame);
 		cv::waitKey(1);
 	}
 		
