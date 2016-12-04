525244ab53f6f32ceec20014e55b4ea6ad39dd4e
byfron <josecrubio82@gmail.com>
2016-12-04 15:46:34 +0100

    Improved detection using contour shape analysis

--- app/cameraStream.cpp
+++ app/cameraStream.cpp
@@ -1,5 +1,10 @@
 #include <camera/Camera.hpp>
 
+using namespace cv;
+
+RNG rng(12345);
+
+
 int main(int argc, char ** argv) {
 
 	Camera camera;
@@ -15,20 +20,52 @@ int main(int argc, char ** argv) {
 		cv::imshow("comp", grayFrame);
 		cv::waitKey(1);
 
+		std::vector< std::vector<cv::Point> > contours;
+		std::vector<cv::Vec4i> hierarchy;
+		Mat mask = grayFrame > 0;
+		cv::findContours(mask, contours, hierarchy, CV_RETR_TREE,
+				 CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+		
 		cv::Mat labels;
 		int num_comp = cv::connectedComponents(255 - grayFrame, labels);
-		std::cout << "num comp "<< num_comp << std::endl;  
-		cv::normalize(labels, labels, 0, 255, cv::NORM_MINMAX, CV_8U);
 
-	
-		cv::Mat colored_labels;
-		// labels.convertTo(labels, CV_32F);
-		// cv::normalize(labels, labels, 0.0, 1.0, cv::NORM_MINMAX, CV_32F);
-		cvtColor(labels, labels, CV_GRAY2RGB);
+		 /// Draw contours with inner contour
+		std::vector<Rect> boundRect( contours.size() );
+		std::vector<std::vector<Point> > contours_poly( contours.size() );
+		std::vector<float>radius( contours.size() );
+		std::vector<Point2f>center( contours.size() );
 
-		cv::applyColorMap(labels, colored_labels, cv::COLORMAP_JET);
-		cv::imshow("comp", colored_labels);
+		Mat drawing = Mat::zeros(grayFrame.size(), CV_8UC3 );
+		for( int i = 0; i< contours.size(); i++ )
+		{
+			if (hierarchy[i][2] > 0) {
+
+				float  epsilon = 0.05*arcLength(contours[i],true);
+				approxPolyDP( Mat(contours[i]), contours_poly[i], epsilon, true );
+				boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+
+				if (contours_poly[i].size() == 4)
+				{
+				// get bounding box
+				Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+				drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+				}
+			}
+
+
+		}		
+
+		cv::imshow("contours", drawing);
 		cv::waitKey(1);
+		
+		// cv::Mat colored_labels;
+		// // labels.convertTo(labels, CV_32F);
+		// // cv::normalize(labels, labels, 0.0, 1.0, cv::NORM_MINMAX, CV_32F);
+		// cvtColor(labels, labels, CV_GRAY2RGB);
+
+		// cv::applyColorMap(labels, colored_labels, cv::COLORMAP_JET);
+		// cv::imshow("comp", colored_labels);
+		// cv::waitKey(1);
 	}
 		
 	return 0;
